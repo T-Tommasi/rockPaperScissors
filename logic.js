1292b652b5a02bc8981afc31c8ec9bb3cbdc377e
T-Tommasi <ttommasi1945@gmail.com>
2024-04-28 09:36:34 +0200

    (feat) refactored DOM manipulation
    Added actually functioning DOM manipulation
    Added more streamlined logic for the game
    Fixed win\lose condition
    Fixed const name for clarity
    Fixed some variables missing their correct ID
    Fixed missing DOM calls

--- logic.js
+++ logic.js
@@ -5,10 +5,20 @@ let computerScore = 0;
 const WIN_MESSAGE = `You won! `;
 const LOSE_MESSAGE = `You lost! `;
 const DRAW_MESSAGE = `It's a draw! `;
-const statusDiv = document.querySelector('.gameStatus');
+const CONTAINER = document.querySelector('.container')
+const WIN_LOSE_CONDITION = document.createElement('div');
+const SCORE_PLAYER = document.createElement('div');
+SCORE_PLAYER.textContent = `Player: ${playerScore}`;
+const SCORE_COMPUTER = document.createElement('div');
+SCORE_COMPUTER.textContent = `Computer: ${computerScore}`;
 const PLAYER_ROCK = document.querySelector('#rock');
 const PLAYER_PAPER = document.querySelector('#paper');
 const PLAYER_SCISSORS = document.querySelector('#scissors');
+
+//Append to the DOM the score counter and win status
+CONTAINER.appendChild(WIN_LOSE_CONDITION);
+CONTAINER.appendChild(SCORE_PLAYER);
+CONTAINER.appendChild(SCORE_COMPUTER);
 
 //Function for choosing a random answer from the array
 function pcResponse (ANSWERS) {
@@ -24,48 +34,52 @@ function playRound(pcAnswer,playerAnswer) {
     if (playerAnswer == 'rock' && pcAnswer == 'scissors' ||
     playerAnswer == 'paper' && pcAnswer == 'rock' ||
     playerAnswer == 'scissors' && pcAnswer == 'paper') {
-        return {message: WIN_MESSAGE + playerAnswer + 'beat ' + pcAnswer, status: win}
+        return {message: WIN_MESSAGE + playerAnswer + ' beat ' + pcAnswer, status: WIN_MESSAGE}
     } else if (playerAnswer == 'rock' && pcAnswer == 'paper' ||
     playerAnswer == 'paper' && pcAnswer == 'scissors' ||
     playerAnswer == 'scissors' && pcAnswer == 'rock') {
-        return {message: LOSE_MESSAGE + pcAnswer + 'beat ' + playerAnswer, status: lose}
-    } else {
-        return {message: DRAW_MESSAGE + pcAnswer + 'is the same as ' + playerAnswer, status: draw}
+        return {message: LOSE_MESSAGE + pcAnswer + ' beat ' + playerAnswer, status: LOSE_MESSAGE}
+    } else if (playerAnswer == pcAnswer) {
+        return {message: DRAW_MESSAGE + pcAnswer + ' is the same as ' + playerAnswer, status: DRAW_MESSAGE}
     }
-}
+};
 
 //This function starts a game of RPS
 function playGame (playerAnswer) {
-    let result = playRound(playerAnswer,pcResponse(answers));
-    if (result.includes('You won')) {
-        statusDiv.textContent = WIN_MESSAGE.message
-        return ++playerScore
-    } else if (result.includes('You lost')) {
-        statusDiv.textContent = LOSE_MESSAGE.message
-        return ++computerScore
+    let result = playRound(pcResponse(ANSWERS),playerAnswer);
+    if (result.message.includes('You won! ')) {
+        WIN_LOSE_CONDITION.textContent = result.message;
+        ++playerScore
+    } else if (result.message.includes('You lost! ')) {
+        WIN_LOSE_CONDITION.textContent = result.message;
+        ++computerScore
     } else {
-        statusDiv.textContent = DRAW_MESSAGE.message
+        WIN_LOSE_CONDITION.textContent = result.message
     }
     if (playerScore == 5) {
         alert('You won the game, congratulations!')
         playerScore = 0
         computerScore = 0
-        return
     } else if  (computerScore == 5) {
         alert('Damn! you lost the game!')
         computerScore = 0
         playerScore = 0
-        return
-    }
-}
+    };
+    console.log(playerAnswer);
+    console.log(result);
+    console.log(playerScore, computerScore);
+    SCORE_PLAYER.textContent = `Player: ${playerScore}`;
+    SCORE_COMPUTER.textContent = `Computer: ${computerScore}`;
+    return;
+};
 
 //Start the game and use the button selected by the player as his choiche of play
-PLAYER_ROCK.addEventListener('click', function () {
+PLAYER_ROCK.addEventListener('click', function() {
     playGame('rock')
 });
-PLAYER_PAPER.addEventListener('click', function () {
+PLAYER_PAPER.addEventListener('click', function() {
     playGame('paper')
 });
-PLAYER_SCISSORS.addEventListener('click', function () {
+PLAYER_SCISSORS.addEventListener('click', function() {
     playGame('scissors')
-})
+});
